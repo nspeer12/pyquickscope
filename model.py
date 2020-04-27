8a5c93b8f3422b4beb986529e2f80d09aa898b6d
nspeer12 <nspeer252@gmail.com>
2020-04-27 17:14:01 -0400

    data loaders and training workinggggg

--- model.py
+++ model.py
@@ -1,76 +1,85 @@
 import os
+import glob
+import datetime
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.applications import Xception
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator # causing errors
 from tensorflow.keras import optimizers
+
 
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 sess = tf.compat.v1.Session(config=config)
 
-class Model:
-    def __init__(self):
-        self.base_dir = 'data/'
-        self.train_dir = os.path.join(self.base_dir, 'train')
-        self.validation_dir = os.path.join(self.base_dir, 'validation')
-        self.train_humans_dir = os.path.join(self.train_dir, 'humans')
-        self.train_other_dir = os.path.join(self.train_dir, 'other')
-        self.validation_humans_dir = os.path.join(self.validation_dir, 'human')
-        self.validation_other_dir = os.path.join(self.validation_dir, 'other')
+base_dir = 'data\\'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+train_humans_dir = os.path.join(train_dir, 'humans')
+train_other_dir = os.path.join(train_dir, 'other')
+validation_humans_dir = os.path.join(validation_dir, 'human')
+validation_other_dir = os.path.join(validation_dir, 'other')
 
-        self.conv_base = Xception(weights='imagenet', 
-                             include_top=False, 
-                             input_shape=(300, 300, 3))
+# model paths
+model_dir = os.path.dirname('models\\')
+checkpoint_path = os.path.join(model_dir, 'checkpoints/')
+log_dir = os.path.join(base_dir, 'logs\\')
 
-        self.conv_base.trainable = False
-        self.model = keras.models.Sequential()
-        self.model.add(self.conv_base)
-        self.model.add(keras.layers.Flatten())
-        self.model.add(keras.layers.Dense(512, activation='relu'))
-        self.model.add(keras.layers.Dense(32, activation='relu'))
-        self.model.add(keras.layers.Dense(1, activation='sigmoid'))
+def create_model():
+    conv_base = Xception(weights='imagenet', include_top=False, input_shape=(300, 300, 3))
+    conv_base.trainable = False
+    model = keras.models.Sequential()
+    model.add(conv_base)
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(512, activation='relu'))
+    model.add(keras.layers.Dense(32, activation='relu'))
+    model.add(keras.layers.Dense(1, activation='sigmoid'))
+    
+    model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=2e-5), metrics=['acc'])
 
-    def load_data(self):
-        train_datagen = ImageDataGenerator(
-                        rescale=1./255, 
-                        rotation_range=40,
-                        width_shift_range=0.2,
-                        height_shift_range=0.2,
-                        shear_range=0.2,
-                        zoom_range=0.2,
-                        horizontal_flip=True,
-                        fill_mode='nearest'
-                        )
+    return model
+
+def train(model):
+    # data loaders
+    train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=40, width_shift_range=0.2,
+                                       height_shift_range=0.2, shear_range=0.2, zoom_range=0.2,
+                                       horizontal_flip=True, fill_mode='nearest')
         
-        self.train_generator = train_datagen.flow_from_directory(
-                            self.train_dir,
-                            target_size=(150, 150),
-                            batch_size=20,
-                            class_mode='binary')
+    train_generator = train_datagen.flow_from_directory(train_dir, target_size=(300, 300),
+                                                        batch_size=20, class_mode='binary')
         
-        validation_datagen = ImageDataGenerator(rescale=1./255)
+    validation_datagen = ImageDataGenerator(rescale=1./255)
 
-        self.validation_generator = validation_datagen.flow_from_directory(
-                                self.validation_dir,
-                                target_size=(150, 150),
-                                batch_size=20,
-                                class_mode='binary')
-        
-    def build_model(self):
-        self.model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=2e-5), metrics=['acc'])
+    validation_generator = validation_datagen.flow_from_directory(validation_dir, target_size=(300, 300),
+                                                                  batch_size=20, class_mode='binary')
+    
+    # checkpoints
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True, verbose=1)
 
-        # train
-        history = self.model.fit_generator(self.train_generator, steps_per_epoch=100, epochs=100,
-                                      validation_data=self.validation_generator, validation_steps=50)
+    # tensorboard
+    log = log_dir + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log, histogram_freq=10)
 
+    # callbacks
+    callbacks = [checkpoint_callback, tensorboard_callback]
+
+    # train
+    history = model.fit_generator(train_generator, steps_per_epoch=25, epochs=10,
+                                  validation_data=validation_generator, validation_steps=25,
+                                  callbacks=callbacks)
+
+    return model
+
+
+def data_loader():
+    #classes = np.array([item.name for item in train_dir.glob('*')])
+    print(glob.glob(train_dir + str('*')))
+    #print(classes)
 
 def main():
-    model = Model()
-    model.load_data()
-    model.build_model()
-
-
+    model = create_model()
+    train(model)
 
 if __name__=='__main__':
     main()
