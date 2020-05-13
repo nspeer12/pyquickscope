1a063d4d7e29bff80475f94e6137f02f63bec2e7
nspeer12 <nspeer252@gmail.com>
2020-05-13 10:53:53 -0400

    automatic data cleaning

--- data-cleaner.py
+++ data-cleaner.py
@@ -4,6 +4,10 @@ import glob
 import time
 import shutil
 import random
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from model import *
 
 # base directories
 data_dir = 'data\\'
@@ -13,58 +17,117 @@ train_dir = os.path.join(data_dir, 'train\\')
 val_dir = os.path.join(data_dir, 'validation\\')
 is_player_dir = os.path.join(train_dir, 'player\\')
 is_other_dir = os.path.join(train_dir, 'other\\')
-
-# data filtering directories
 raw_player_dir = os.path.join(raw_dir, 'player\\')
 raw_other_dir = os.path.join(raw_dir, 'other\\')
-maybe_player_dir = os.path.join(raw_player_dir, 'suspected\\')
-maybe_not_player_dir = os.path.join(raw_player_dir, 'unsuspected\\')
-maybe_other_dir = os.path.join(raw_other_dir, 'suspected\\')
-maybe_not_other_dir = os.path.join(raw_other_dir, 'unsuspected\\')
 
+# data filtering directories
+prob_90_player = 'data\\sorting\\prob_90_player\\'
+prob_80_player = 'data\\sorting\\prob_80_player\\'
+prob_70_player = 'data\\sorting\\prob_70_player\\'
+prob_60_player = 'data\\sorting\\prob_60_player\\'
+prob_50_player = 'data\\sorting\\prob_50_player\\'
+prob_40_player = 'data\\sorting\\prob_40_player\\'
+prob_30_player = 'data\\sorting\\prob_30_player\\'
+prob_20_player = 'data\\sorting\\prob_20_player\\'
+prob_10_player = 'data\\sorting\\prob_10_player\\'
+prob_not_player = 'data\\sorting\\prob_not_player\\'
+
+
+# model directory
+model_path = 'models\\MobileNet100_epochs.2e-05_lr'
 
 def human_in_the_loop_labeling(classes=['player', 'other']):
     # look at maybe directories and manually sort
     # save images again in train/validation directory
+
+    dirs = [prob_90_player, prob_80_player, prob_70_player, prob_60_player, prob_60_player,
+    prob_50_player, prob_40_player, prob_30_player, prob_20_player, prob_10_player, prob_not_player]
+
+    for d in dirs:
+        frames = glob.glob(d + '*.jpg')
+        
+        for f in frames:
+            # rename the file when moving
+            new_name = str(random.randint(0, 999999)) + '.jpg'
+
+            img = cv2.imread(f)
+            cv2.imshow(f, img)
+            
+            # keyboard input
+            key = cv2.waitKey(0)
+
+            # label image and move file
+            if key == ord('b'):
+                # label as other
+                new_path = os.path.join(is_other_dir, new_name)
+                shutil.move(f, new_path)
+                print(f, ' labeled as other and moved to ', new_path)
+
+            elif key == ord('v'):
+                # label as player
+                new_path = os.path.join(is_player_dir, new_name)
+                shutil.move(f, new_path)
+                print(f, ' labeled as player', new_path)
+            
+            elif key == ord('n'):
+                # move to rubbish
+                new_path = os.path.join(rubbish_dir, new_name)
+                shutil.move(f, new_path)
+                print(f, ' moved to rubbish ', new_path)
+            
+            elif key == ord('q'):
+                break
+
+            cv2.destroyAllWindows()
+
+def label_with_model():
+    # lower bound for model confidence to label as a human
+    
+    model = load_model(model_path)
+    
     frames = glob.glob(raw_player_dir + '*.jpg')
     
     for f in frames:
-        # rename the file when moving
-        new_name = str(random.randint(0, 999999)) + '.jpg'
-
-        img = cv2.imread(f)
-        cv2.imshow(f, img)
+        # load image
+        img = Image.open(f)
+        i = tf.keras.preprocessing.image.img_to_array(img)
+        i = tf.keras.applications.mobilenet_v2.preprocess_input(i)
         
-        # keyboard input
-        key = cv2.waitKey(0)
-
-        # label image and move file
-        if key == ord('b'):
-            # label as other
-            new_path = os.path.join(is_other_dir, new_name)
-            shutil.move(f, new_path)
-            print(f, ' labeled as other and moved to ', new_path)
-
-        elif key == ord('v'):
-            # label as player
-            new_path = os.path.join(is_player_dir, new_name)
-            shutil.move(f, new_path)
-            print(f, ' labeled as player', new_path)
+        # make predictions
+        pred = model.predict(np.array([i]))
+        print(pred)
         
-        elif key == ord('n'):
-            # move to rubbish
-            new_path = os.path.join(rubbish_dir, new_name)
-            shutil.move(f, new_path)
-            print(f, ' moved to rubbish ', new_path)
-        
-        elif key == ord('q'):
-            break
+        # assign new random name
+        img_name = str(random.randint(0, 999999)) + '.jpg'
 
-        cv2.destroyAllWindows()
+        # filter into directories
+        if pred > 0.9:
+            img.save(prob_90_player + img_name)
+        elif pred > 0.8:
+            img.save(prob_80_player + img_name)
+        elif pred > 0.7:
+            img.save(prob_70_player + img_name)
+        elif pred > 0.6:
+            img.save(prob_60_player + img_name)
+        elif pred > 0.5:
+            img.save(prob_50_player + img_name)
+        elif pred > 0.4:
+            img.save(prob_40_player + img_name)
+        elif pred > 0.3:
+            img.save(prob_30_player + img_name)
+        elif pred > 0.2:
+            img.save(prob_20_player + img_name)
+        elif pred > 0.1:
+            img.save(prob_10_player + img_name)
+        else:
+            img.save(prob_not_player + img_name)
+        
+        os.remove(f)
+
+
 
 def main():
     human_in_the_loop_labeling()
-
     return
 
 
--- data-collection.py
+++ data-collection.py
@@ -7,26 +7,28 @@ from vision import get_roi, screenshot
 import matplotlib.pyplot as plt
 from pynput import keyboard
 from pynput.keyboard import Key, Controller
+from pynput.mouse import Listener
 import random
+import time
 
 window = [0, 0, 1920, 1080]
 n = 300
 roi = get_roi(window, n)
 
-other_dir = "data\\raw\\fresh-other\\"
-human_dir = "data\\raw\\human\\"
+other_dir = "data\\raw\\other\\"
+human_dir = "data\\raw\\player\\"
 
 def on_press(key):
     img_name = str(random.randint(0, 999999)) + '.jpg'
 
     try:
-        if key.char == 'q':
+        if key.char == 'x':
             exit(0)
-        if key.char == 'v':
+        if key.char == 'q':
             print('person')
             img = screenshot(roi)
             img.save(human_dir + img_name)
-        if key.char == 'b':
+        if key.char == 'shift':
             print('other')
             img = screenshot(roi)
             img.save(other_dir + img_name)
@@ -40,16 +42,32 @@ def on_release(key):
         return False
 
 
+
+def on_click(x, y, button, pressed):
+    '''
+    img_name = str(random.randint(0, 999999)) + '.jpg'
+    if pressed:
+        img = screenshot(roi)
+        img.save(human_dir + img_name)
+        print('player caputered: ', img_name)
+    '''
+    return
+
+def on_scroll(x, y, button, pressed):
+    img_name = str(random.randint(0, 999999)) + '.jpg'
+    if pressed:
+        img = screenshot(roi)
+        img.save(other_dir + img_name)
+        print('player caputered: ', img_name)
+
 if __name__=='__main__':
+    # get back into the game
+    for i in range(5):
+        print('waiting for ', 5 - i, ' seconds')
+        time.sleep(1)
+
     # define window and region
     
     # create keyboard listener
-    with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-            listener.join()
-
-    listener = keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release)
-    listener.start()
+    with Listener(on_click=on_click, on_scroll=on_scroll) as listener:
+        listener.join() 
